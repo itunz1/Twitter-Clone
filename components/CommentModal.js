2e88fd47278b86fbc8b076b721d406cb628e00e4
itunz1 <stx_jose@hotmail.com>
2023-09-08 09:57:08 -0400

    add comment section

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -1,12 +1,94 @@
 import { useRecoilState } from "recoil"
-import { modalState } from "../atom/modalAtom"
+import { modalState, postIdState } from "../atom/modalAtom"
+import Modal from "react-modal"
+import { EmojiHappyIcon, PhotographIcon, XIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+import { db } from "@/firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import Moment from "react-moment";
 
 function CommentModal() {
-    const [open, setOpen] = useRecoilState(modalState);
+  const [open, setOpen] = useRecoilState(modalState);
+  const [postId] = useRecoilState(postIdState);
+  const [post, setPost] = useState({});
+  const [input, setInput] = useState("");
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    onSnapshot(doc(db, "posts", postId), (snapshot) => {
+      setPost(snapshot);
+    });
+  }, [postId, db]);
+
+  function sendComment() {
+
+  }
 
   return (
     <div>
-        
+      {open && (
+        <Modal isOpen={open}
+          onRequestClose={() => setOpen(false)}
+          className="max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-400 rounded-xl shadow-md"
+        >
+          <div className="p-1">
+            <div className="border-b border-gray-200 py-2 px-1.5">
+              <div onClick={() => setOpen(false)} className="flex items-center justify-center w-10 h-10 hoverEffect">
+                <XIcon className="p-0 text-gray-700 h-[23px]" />
+              </div>
+            </div>
+            <div className="relative flex items-center p-2 space-x-1">
+              <span className="w-0.5 h-full z-[-1] absolute left-8 top-11 bg-gray-300" />
+              <img className="mr-4 rounded-full h-11 w-11" src={post?.data()?.userImg} />
+              <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post?.data()?.name}</h4>
+              <span className='text-sm sm:text-[15px]'>@{post?.data()?.username} -</span>
+              <span className='text-sm sm:text-[15px] hover:underline'>
+                <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
+              </span>
+            </div>
+            <p className="text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2">
+              {post?.data()?.text}
+            </p>
+
+            <div className='flex p-3 space-x-3'>
+              <img src={session.user.image} alt="user-img"
+                className='rounded-full cursor-pointer h-11 w-11 hover:brightness-95'
+              />
+              <div className='w-full divide-y divide-gray-200'>
+                <div className=''>
+                  <textarea
+                    className='w-full text-lg tracking-wide border-none focus:ring-0 placeholder:gray-700 min-h-[50px] text-gray-700'
+                    rows='2'
+                    placeholder="Tweet your reply"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  />
+                </div>
+
+                <div className='flex items-center justify-between pt-2.5'>
+                  <div className='flex'>
+                    <div className=''
+                    // onClick={() => filePickerRef.current.click()}
+                    >
+                      <PhotographIcon className='w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100' />
+                      {/* <input type='file' hidden ref={filePickerRef} onChange={addImageToPost} /> */}
+                    </div>
+                    <EmojiHappyIcon className='w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100' />
+                  </div>
+                  <button
+                    className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+                    disabled={!input.trim()}
+                    onClick={sendComment}
+                  >
+                    Reply
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -11,8 +11,8 @@ import { signIn, useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import Moment from 'react-moment'
 import { deleteObject, ref } from 'firebase/storage'
-import { useRecoilState,  } from 'recoil'
-import { modalState } from '@/atom/modalAtom'
+import { useRecoilState, } from 'recoil'
+import { modalState, postIdState } from '@/atom/modalAtom'
 
 
 
@@ -22,34 +22,35 @@ function Posts({ post }) {
   const [likes, setLikes] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "posts", post.id, "likes"),
+      onSnapshot(collection(db, "posts", post.id, "likes"),
       (snapshot) => setLikes(snapshot.docs))
   }, [db]);
 
   useEffect(() => {
-    setHasLiked(likes.findIndex((like)=> like.id === session?.user.uid) !== -1)
+    setHasLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1)
   }, [likes])
 
   async function likePost() {
-    if(session){
-      if(hasLiked){
+    if (session) {
+      if (hasLiked) {
         await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid))
-      }else {
+      } else {
         await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
           username: session.user.username,
         })
       }
-    }else {
+    } else {
       signIn()
     }
   };
 
   async function deletePost() {
-    if(window.confirm('Are you sure you want to delete this post?')){
+    if (window.confirm('Are you sure you want to delete this post?')) {
       deleteDoc(doc(db, "posts", post.id))
-      if(post.data().image){
+      if (post.data().image) {
         deleteObject(ref(storage, `posts/${post.id}/image`))
       }
     }
@@ -71,21 +72,30 @@ function Posts({ post }) {
 
         </div>
         <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post.data().text}</p>
-        <img className='mr-2 rounded-2xl' src={post.data().image}/>
+        <img className='mr-2 rounded-2xl' src={post.data().image} />
 
         <div className='flex justify-between p-2 text-gray-500'>
-          <ChatIcon onClick={()=> setOpen(!open)} className='p-2 h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100' />
+          <ChatIcon onClick={() => {
+            if (!session) {
+              signIn();
+            } else {
+              setPostId(post.id)
+              setOpen(!open);
+            }
+          }}
+            className='p-2 h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100'
+          />
           {session?.user.uid === post?.data().id && (
             <TrashIcon onClick={deletePost} className='p-2 h-9 w-9 hoverEffect hover:text-red-600 hover:bg-red-100' />
           )}
           <div className='flex items-center'>
-          {hasLiked ? 
-          (<HeartIconFilled onClick={likePost} className='p-2 text-red-600 h-9 w-9 hoverEffect hover:bg-red-100' />) : 
-          ( <HeartIcon onClick={likePost} className='p-2 h-9 w-9 hoverEffect hover:text-red-600 hover:bg-red-100'/> )}
-          {likes.length > 0 && (
+            {hasLiked ?
+              (<HeartIconFilled onClick={likePost} className='p-2 text-red-600 h-9 w-9 hoverEffect hover:bg-red-100' />) :
+              (<HeartIcon onClick={likePost} className='p-2 h-9 w-9 hoverEffect hover:text-red-600 hover:bg-red-100' />)}
+            {likes.length > 0 && (
               <span className={`${hasLiked && "text-red-600"} text-sm select-none`}>{likes.length}</span>
             )
-          }
+            }
           </div>
           <ShareIcon className='p-2 h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100' />
           <ChartBarIcon className='p-2 h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100' />
